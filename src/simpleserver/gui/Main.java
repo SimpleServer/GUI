a076216d0dfb5c2e0c7323f9149d91e6d26897b6
Simon Marti <simon@ceilingcat.ch>
2011-06-18 20:55:23 +0200

    initial commit

---
+++ src/simpleserver/gui/Main.java
@@ -0,0 +1,10 @@
+package simpleserver.gui;
+
+import simpleserver.Server;
+
+public class Main {
+  public static void main(String[] args) {
+    System.out.println("SimpleServer GUI 0.0");
+    Server server = new Server();
+  }
+}
